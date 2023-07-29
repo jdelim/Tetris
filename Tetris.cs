e30bea606d09cff5971dd783a904bc82b09f4dce
Jadriel Delim <jadrieldelim@gmail.com>
2023-07-29 00:30:42 -0400

    Initialized Tetris.cs with TODOs

---
+++ Tetris.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+public class Tetris
+{
+    bool[,] Board;
+    int[] DestroyedRows;
+
+    public Tetris()
+    {
+        // TODO: finish constructor
+    }
+    // TODO: create method to place blocks
+
+    // TODO: create method to detect a line of blocks
+
+    // TODO: create method which checks the board 
+
+    // TODO: create method which clears a line
+
+    // TODO: create method which destroys full rows
+
+    // TODO: create a method which moves board down after blocks destroyed
+
+    // TODO: create a method which updates the board
+}

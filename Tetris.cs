b43f2b81b7952afa312f103a140b048acb692664
Macaroniwa <marcusethanwang@gmail.com>
2023-07-31 13:26:27 -0400

    places block

--- Tetris.cs
+++ Tetris.cs
@@ -16,19 +16,11 @@ public class Tetris
     private int[] DestroyedRows;
     public Tetris()
     {
-        // TODO: finish constructor
+        
     }
     // TODO: create method to place blocks
-
-    // TODO: create method to detect a line of blocks
-
-    // TODO: create method which checks the board 
-
-    // TODO: create method which clears a line
-
-    // TODO: create method which destroys full rows
-
-    // TODO: create a method which moves board down after blocks destroyed
-
-    // TODO: create a method which updates the board
+    public void Move(int x, int y, TetrominoType aColor)
+    {
+        Board[x, y] = new Block(aColor);
+    }
 }

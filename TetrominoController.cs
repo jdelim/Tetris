a23d2cdc13438d9f508ebcfc1e09f4f25e542216
WillSchl <WillTheWizlock@outlook.com>
2023-08-01 13:46:08 -0400

    Started the first part of the rotations

--- TetrominoController.cs
+++ TetrominoController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TetrominoMaker;
 
 public class TetrominoController : MonoBehaviour
 {
+    public Rotation;
     //TODO: Create a Vector2Int Covering the Origin Position
+    public Vector2int() OriginPosition;
     //TODO: Create a set of four Vector2Ints that are for Rotation 1
+    public Vector2int() Rotation1
     //TODO: Create a set of four Vector2Ints that are for Rotation 2
+    public Ve
     //TODO: Create a set of four Vector2Ints that are for Rotation 3
     //TODO: Create a set of four Vector2Ints that are for Rotation 4
     //TODO: *Create a method to check if a specific set of positions is available on the board

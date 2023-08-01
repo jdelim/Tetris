d0bdd1851677035d004a70620fb506e9c5b1b24f
WillSchl <WillTheWizlock@outlook.com>
2023-08-01 14:24:51 -0400

    Trying to fix conflicts

--- TetrominoController.cs
+++ TetrominoController.cs
@@ -13,6 +13,7 @@ public class TetrominoController : MonoBehaviour
     public Rotation LSpin4 = new Rotation(0, 1, 0, -1, -1, 1, 0, 0);
     public Tetromino LTetromino = new Tetromino(Vector2Int.zero, LSpin1, LSpin2, LSpin3, LSpin4);
     #endregion
+<<<<<<< HEAD
     //TODO: Create a Vector2Int Covering the Origin Position
     //TODO: Create a set of four Vector2Ints that are for Rotation 1
     //TODO: Create a set of four Vector2Ints that are for Rotation 2
@@ -27,6 +28,22 @@ public class TetrominoController : MonoBehaviour
 >>>>>>> STetromino
     //TODO: Create a set of four Vector2Ints that are for Rotation 3
     //TODO: Create a set of four Vector2Ints that are for Rotation 4
+=======
+    #region Z Tetromino
+    public Rotation ZSpin1 = new Rotation(-1, 1, 0, 1, 1, 0, 0, 0);
+    public Rotation ZSpin2 = new Rotation(0, -1, 1, 0, 1, 1, 0, 0);
+    public Rotation ZSpin3 = new Rotation(-1, 0, 0, -1, 1, -1, 0, 0);
+    public Rotation ZSpin4 = new Rotation(-1, -1, -1, 0, 0, 1, 0, 0);
+    public Tetromino ZTetromino = new Tetromino(Vector2Int.zero, ZSpin1, ZSpin2, ZSpin3, ZSpin4);
+    #endregion
+    #region Line Tetromino
+    public Rotation LineSpin1 = new Rotation(0, 1, 1, 1, 2, 1, 3, 1);
+    public Rotation LineSpin2 = new Rotation(2, 0, 2, 1, 2, 2, 2, 3);
+    public Rotation LineSpin3 = new Rotation(0, 2, 1, 2, 2, 2, 3, 2);
+    public Rotation LineSpin4 = new Rotation(1, 0, 1, 1, 1, 2, 1, 3);
+    public Tetromino LineTetromino = new Tetromino(Vector2Int.zero, LineSpin1, LineSpin2, LineSpin3, LineSpin4);
+    #endregion
+>>>>>>> 867654b567deaf63dd96a4c96cb81461a71b626e
     //TODO: *Create a method to check if a specific set of positions is available on the board
     //TODO: Create a means to Access the Board
     //TODO: Create a method to drop tetrominos every (preset) time
